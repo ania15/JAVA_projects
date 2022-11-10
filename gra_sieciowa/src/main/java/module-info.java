df674e3789f8a3d9b86f1b2aa0fddfbe88df9949
ania15 <annakaniowska7@gmail.com>
2022-11-10 10:36:07 +0100

    game

--- gra_sieciowa/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.gra_sieciowa {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.gra_sieciowa to javafx.fxml;
-    exports com.example.gra_sieciowa;
-}
